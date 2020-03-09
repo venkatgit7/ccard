24e3bcb6d5ab358ddb2439647ca37f560315f7db
venkatgit7 <36585894+venkatgit7@users.noreply.github.com>
2020-03-09 19:40:15 +0530

    updated

--- src/main/java/com/dbs/App.java
+++ src/main/java/com/dbs/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Venkat" );
+        System.out.println( "Hello Venkat welcome to java" );
     }
 }
